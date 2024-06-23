171dd513d19fb0565625f8c8c0f69c0f3c11df2c
publicprofile123 <145284020+kukuffsh@users.noreply.github.com>
2024-06-23 10:04:11 +0300

    Update main.py

--- backend/api/main.py
+++ backend/api/main.py
@@ -18,4 +18,5 @@ app.add_middleware(
 async def process_text(data: UploadTextData) -> Result:
     answers = AI_or_Human(data.text)
     out_text = mlGOVNO(data.text)
-    return Result(human = answers[0][0], ai = answers[0][1], text = out_text)
+    answers2 = AI_or_Human(out_text.text)
+    return Result(original_percent = answers[0][0], rewrite_percent = answers[0][1], text = out_text)
